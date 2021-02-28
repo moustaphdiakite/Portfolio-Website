9e1cbed47387601b674f2ef1ca7f610259fa9ea6
Moustaph Diakite <mdiakite97@gmail.com>
2021-02-27 19:58:25 -0500

    Reset All Work

--- script.js
+++
@@ -1,9 +0,0 @@
-let imgTarget = document.getElementById("image1");
-let imgTarget2 = document.getElementById("image2");
-
-function summaryDisplay() {
-    document.getElementById("project-summary").style.display = "flex"
-}
-
-imgTarget.addEventListener("mouseover", summaryDisplay);
-imgTarget2.addEventListener("mouseover", summaryDisplay);
